5f213eca4101e640f5220c8f99e721ac2a7a4c5a
ElliotLeeLL <xingyuli1996@gmail.com>
2025-06-17 15:03:15 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,29 @@
-print("hello world")
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct",
+    device_map="cuda",
+    torch_dtype="auto",
+    trust_remote_code=True
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+generator = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=False,
+    max_new_tokens=500,
+    do_sample=False,
+)
+
+message = [
+    {"role": "user", "content": "How many states there in the USA?"}
+]
+
+output = generator(message)
+print(output[0]["generated_text"])
