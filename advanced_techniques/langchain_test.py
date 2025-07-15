7ed1c65c5500b53ac0a4459654058dafcd2070a6
ElliotLeeLL <xingyuli1996@gmail.com>
2025-07-15 13:16:53 +0800

    Update langchain_test.py

--- advanced_techniques/langchain_test.py
+++ advanced_techniques/langchain_test.py
@@ -1,1 +1,15 @@
-from langchain import LlamaCpp, PromptTemplate
+from langchain import PromptTemplate
+from langchain_community.llms import LlamaCpp
+
+
+model_path = "Phi-3-mini-4k-instruct-fp16.gguf"
+llm = LlamaCpp(
+    model_path=model_path,
+    n_gpu_layers=-1,
+    max_tokens=500,
+    n_ctx=2048,
+    seed=43,
+    verbose=False
+)
+res = llm.invoke("Am I cool?")
+print(res)
